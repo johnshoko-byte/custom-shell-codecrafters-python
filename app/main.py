33e4078ce779a2647f8085b9f17122a0fe09aac8
John Shoko <johnshoko8@gmail.com>
2026-03-31 13:27:55 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -12,6 +12,11 @@ def main():
             break
         elif command.startswith("echo "):
             print(command[5:])
+        elif command.startswith("type "):
+            if command[5:] in ["echo", "exit", "type"]:
+                print(f"{command[5:]} is a shell builtin")
+            else:
+                print(f"{command[5:]}: not found")
         else:
             print(f"{command}: command not found")
 
